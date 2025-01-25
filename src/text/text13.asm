9ad364278bbd199ebab6b7c74a9ff34f86e50bc0
Telefoan <155188161+Telefoan@users.noreply.github.com>
2025-01-25 13:08:46 -0500

    text overflow issue fix

--- src/text/text13.asm
+++ src/text/text13.asm
@@ -214,3 +214,13 @@ ReversalDescription:
 WouldYouLikeToConcedeText:
 	text "     Would you like to concede?"
 	done
+
+CurseDescription:
+	text "As many times during your turn "
+	line "you may move 1 damage counter"
+	line "from 1 of your opponent's"
+	line "Pokémon to another (even if it would"
+	line "Knock Out the other Pokémon)."
+	line "This power can't be used if Gengar"
+	line "is Asleep, Confused, or Paralyzed."
+	done
