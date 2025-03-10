ElectroDeoxys/Pokemon-TCG-Extended

Tail call optimization

src/engine/duel/ai/hand_pokemon.asm
@@ -424,12 +424,10 @@ AIDecideSpecialEvolutions:
 	cp 6
 	jr c, .not_enough_energy
 	ld a, 3
-	call AddToAIScore
-	ret
+	jp AddToAIScore
 .not_enough_energy
 	ld a, 10
-	call SubFromAIScore
-	ret
+	jp SubFromAIScore
 
 ; check if Magikarp is not the active card
 ; and has at least 2 energy cards attached
@@ -442,8 +440,7 @@ AIDecideSpecialEvolutions:
 	cp 2
 	ret c
 	ld a, 3
-	call AddToAIScore
-	ret
+	jp AddToAIScore
 
 .invincible_ronald
 	ld a, [wLoadedCard2ID]
@@ -457,8 +454,7 @@ AIDecideSpecialEvolutions:
 	or a ; active card
 	ret z
 	ld a, 10
-	call AddToAIScore
-	ret
+	jp AddToAIScore
 
 .legendary_ronald
 	ld a, [wLoadedCard2ID]
@@ -495,8 +491,7 @@ AIDecideSpecialEvolutions:
 	jr c, .check_muk
 .lower_score
 	ld a, 10
-	call SubFromAIScore
-	ret
+	jp SubFromAIScore
 
 ; if there's no Muk, raise score
 .check_muk
@@ -504,8 +499,7 @@ AIDecideSpecialEvolutions:
 	call CountPokemonIDInBothPlayAreas
 	jr c, .lower_score
 	ld a, 10
-	call AddToAIScore
-	ret
+	jp AddToAIScore
 
 ; if Dragonair is active, check its damage in HP
 ; if this result is >= 50,
@@ -604,12 +598,10 @@ AIDecidePlayLegendaryBirds:
 
 ; add
 	ld a, 70
-	call AddToAIScore
-	ret
+	jp AddToAIScore
 .subtract
 	ld a, 100
-	call SubFromAIScore
-	ret
+	jp SubFromAIScore
 
 .moltres
 	; checks if there's enough cards in deck

src/engine/duel/ai/retreat.asm
@@ -531,8 +531,7 @@ AIDecideBenchPokemonToSwitchTo:
 	ld a, [wDamage]
 	call CalculateByteTensDigit
 	inc a
-	call AddToAIScore
-	ret
+	jp AddToAIScore
 
 ; if an energy card that is needed is found in hand
 ; calculate damage of the move and raise AI score

src/engine/menus/deck_machine.asm
@@ -119,14 +119,12 @@ HandleDeckMissingCardsList:
 	ld [hl], e
 	inc hl
 	ld [hl], d
-	call PrintConfirmationCardList
-	ret
+	jp PrintConfirmationCardList
 
 .ClearScreenAndPrintDeckTitle
 	call EmptyScreenAndLoadFontDuelAndHandCardsIcons
 	call .PrintDeckIndexAndName
-	call EnableLCD
-	ret
+	jp EnableLCD
 
 ; prints text in the form "X.<DECK NAME> deck"
 ; where X is the deck index in the list
@@ -161,8 +159,7 @@ HandleDeckMissingCardsList:
 	lb de, 3, 1
 	ld hl, wDefaultText
 	call InitTextPrinting
-	call ProcessText
-	ret
+	jp ProcessText
 
 Data_b04a:
 	db 1 ; x pos
@@ -212,8 +209,7 @@ Func_b088:
 	ld [hl], d
 	ld a, SYM_BOX_RIGHT
 	ld [wCursorAlternateTile], a
-	call PrintCardSelectionList
-	ret
+	jp PrintCardSelectionList
 
 .Func_b0b2
 	ld bc, wTempCardCollection
@@ -311,8 +307,7 @@ PrintCardToSendText:
 	lb de, 1, 1
 	call InitTextPrinting
 	ldtx hl, CardToSendText
-	call ProcessTextFromID
-	ret
+	jp ProcessTextFromID
 
 PrintReceivedTheseCardsText:
 	call EmptyScreenAndDrawTextBox
@@ -327,16 +322,14 @@ PrintReceivedTheseCardsText:
 	ld [wTxRam2 + 0], a
 	ld [wTxRam2 + 1], a
 	ldtx hl, ReceivedTheseCardsFromText
-	call DrawWideTextBox_PrintText
-	ret
+	jp DrawWideTextBox_PrintText
 
 EmptyScreenAndDrawTextBox:
 	call Set_OBJ_8x8
 	call PrepareMenuGraphics
 	lb de, 0, 0
 	lb bc, 20, 13
-	call DrawRegularTextBox
-	ret
+	jp DrawRegularTextBox
 
 HandleDeckSaveMachineMenu:
 	xor a
@@ -643,8 +636,7 @@ ClearScreenAndDrawDeckMachineScreen:
 	call GetSavedDeckPointers
 	call PrintVisibleDeckMachineEntries
 	call GetSavedDeckCount
-	call EnableLCD
-	ret
+	jp EnableLCD
 
 ; prints wDeckMachineTitleText as title text
 SetDeckMachineTitleText:
@@ -654,8 +646,7 @@ SetDeckMachineTitleText:
 	ld a, [hli]
 	ld h, [hl]
 	ld l, a
-	call ProcessTextFromID
-	ret
+	jp ProcessTextFromID
 
 ; save all sSavedDecks pointers in wMachineDeckPtrs
 GetSavedDeckPointers:
@@ -896,8 +887,7 @@ PrintDeckMachineEntry:
 	jr .loop
 .done
 	ld a, c
-	call DisableSRAM
-	ret
+	jp DisableSRAM
 
 ; counts how many decks in sSavedDecks are not empty
 ; stores value in wNumSavedDecks
@@ -920,8 +910,7 @@ GetSavedDeckCount:
 .got_count
 	ld a, e
 	ld [wNumSavedDecks], a
-	call DisableSRAM
-	ret
+	jp DisableSRAM
 
 ; prints "[wNumSavedDecks]/60"
 PrintNumSavedDecks:
@@ -938,8 +927,7 @@ PrintNumSavedDecks:
 	lb de, 14, 1
 	call InitTextPrinting
 	ld hl, wDefaultText
-	call ProcessText
-	ret
+	jp ProcessText
 
 ; handles player choice in what deck to save
 ; in the Deck Save Machine
@@ -1049,8 +1037,7 @@ CheckIfCanBuildSavedDeck:
 	ld l, a
 	ld bc, DECK_NAME_SIZE
 	add hl, bc
-	call CheckIfHasEnoughCardsToBuildDeck
-	ret
+	jp CheckIfHasEnoughCardsToBuildDeck
 
 ; switches to SRAM bank 0 and stores current SRAM bank in wTempBankSRAM
 ; skips if current SRAM bank is already 0
@@ -1239,8 +1226,7 @@ DrawListScrollArrows:
 	ld a, SYM_BOX_RIGHT
 .got_tile_2
 	lb bc, 19, 11
-	call WriteByteToBGMap0
-	ret
+	jp WriteByteToBGMap0
 
 ; handles the deck menu for when the player
 ; needs to make space for new deck to build
@@ -1484,8 +1470,7 @@ TryBuildDeckMachineDeck:
 	call AddDeckToCollection
 	pop hl
 	ld a, DECK_STRUCT_SIZE
-	call ClearNBytesFromHL
-	ret
+	jp ClearNBytesFromHL
 
 ; collects cards missing from player's collection
 ; and shows its confirmation list
@@ -1881,8 +1866,7 @@ HandleAutoDeckMenu:
 	call .CreateAutoDeckPointerList
 	call PrintVisibleDeckMachineEntries
 	call SafelySwitchToSRAM0
-	call EnableLCD
-	ret
+	jp EnableLCD
 
 ; writes to wMachineDeckPtrs the pointers
 ; to the Auto Decks in sAutoDecks

src/home/substatus.asm
@@ -475,8 +475,7 @@ IsClairvoyanceActive::
 	ccf
 	ret nc
 	ld a, OMANYTE
-	call CountPokemonIDInPlayArea
-	ret
+	jp CountPokemonIDInPlayArea
 
 ; returns carry if turn holder's arena card is paralyzed, asleep, confused,
 ; and/or toxic gas in play, meaning that attack and/or pkmn power cannot be used
@@ -762,8 +761,7 @@ HandleDestinyBondSubstatus::
 	ld l, a
 	call LoadTxRam2
 	ldtx hl, KnockedOutDueToDestinyBondText
-	call DrawWideTextBox_WaitForInput
-	ret
+	jp DrawWideTextBox_WaitForInput
 
 ; when MACHAMP is damaged, if its Strikes Back is active, the
 ; attacking Pokemon (turn holder's arena Pokemon) takes 10 damage.

Telefoan/poketcg-extended

src/engine/duel/ai/hand_pokemon.asm
@@ -425,12 +425,10 @@ AIDecideSpecialEvolutions:
 	cp 6
 	jr c, .not_enough_energy
 	ld a, 3
-	call AIEncourage
-	ret
+	jp AIEncourage
 .not_enough_energy
 	ld a, 10
-	call AIDiscourage
-	ret
+	jp AIDiscourage
 
 ; check if Magikarp is not the active card
 ; and has at least 2 energy cards attached
@@ -443,8 +441,7 @@ AIDecideSpecialEvolutions:
 	cp 2
 	ret c
 	ld a, 3
-	call AIEncourage
-	ret
+	jp AIEncourage
 
 .invincible_ronald
 	ld a, [wLoadedCard2ID]
@@ -458,8 +455,7 @@ AIDecideSpecialEvolutions:
 	or a ; active card
 	ret z
 	ld a, 10
-	call AIEncourage
-	ret
+	jp AIEncourage
 
 .legendary_ronald
 	ld a, [wLoadedCard2ID]
@@ -496,8 +492,7 @@ AIDecideSpecialEvolutions:
 	jr c, .check_muk
 .lower_score
 	ld a, 10
-	call AIDiscourage
-	ret
+	jp AIDiscourage
 
 ; if there's no Muk, raise score
 .check_muk
@@ -505,8 +500,7 @@ AIDecideSpecialEvolutions:
 	call CountPokemonWithActivePkmnPowerInBothPlayAreas
 	jr c, .lower_score
 	ld a, 10
-	call AIEncourage
-	ret
+	jp AIEncourage
 
 ; if Dragonair is active, check its damage in HP
 ; if this result is >= 50,
@@ -605,12 +599,10 @@ AIDecidePlayLegendaryBirds:
 
 ; add
 	ld a, 70
-	call AIEncourage
-	ret
+	jp AIEncourage
 .subtract
 	ld a, 100
-	call AIDiscourage
-	ret
+	jp AIDiscourage
 
 .moltres
 	; checks if there's enough cards in deck

src/engine/duel/ai/retreat.asm
@@ -531,8 +531,7 @@ AIDecideBenchPokemonToSwitchTo:
 	ld a, [wDamage]
 	call ConvertHPToDamageCounters_Bank5
 	inc a
-	call AIEncourage
-	ret
+	jp AIEncourage
 
 ; if an energy card that is needed is found in hand
 ; calculate damage of the move and raise AI score

src/engine/menus/deck_machine.asm
@@ -119,14 +119,12 @@ HandleDeckMissingCardsList:
 	ld [hl], e
 	inc hl
 	ld [hl], d
-	call PrintConfirmationCardList
-	ret
+	jp PrintConfirmationCardList
 
 .ClearScreenAndPrintDeckTitle
 	call EmptyScreenAndLoadFontDuelAndHandCardsIcons
 	call .PrintDeckIndexAndName
-	call EnableLCD
-	ret
+	jp EnableLCD
 
 ; prints text in the form "X.<DECK NAME> deck"
 ; where X is the deck index in the list
@@ -161,8 +159,7 @@ HandleDeckMissingCardsList:
 	lb de, 3, 1
 	ld hl, wDefaultText
 	call InitTextPrinting
-	call ProcessText
-	ret
+	jp ProcessText
 
 Data_b04a:
 	db 1 ; x pos
@@ -212,8 +209,7 @@ Func_b088:
 	ld [hl], d
 	ld a, SYM_BOX_RIGHT
 	ld [wCursorAlternateTile], a
-	call PrintCardSelectionList
-	ret
+	jp PrintCardSelectionList
 
 .Func_b0b2
 	ld bc, wTempCardCollection
@@ -311,8 +307,7 @@ PrintCardToSendText:
 	lb de, 1, 1
 	call InitTextPrinting
 	ldtx hl, CardToSendText
-	call ProcessTextFromID
-	ret
+	jp ProcessTextFromID
 
 PrintReceivedTheseCardsText:
 	call EmptyScreenAndDrawTextBox
@@ -327,16 +322,14 @@ PrintReceivedTheseCardsText:
 	ld [wTxRam2 + 0], a
 	ld [wTxRam2 + 1], a
 	ldtx hl, ReceivedTheseCardsFromText
-	call DrawWideTextBox_PrintText
-	ret
+	jp DrawWideTextBox_PrintText
 
 EmptyScreenAndDrawTextBox:
 	call Set_OBJ_8x8
 	call PrepareMenuGraphics
 	lb de, 0, 0
 	lb bc, 20, 13
-	call DrawRegularTextBox
-	ret
+	jp DrawRegularTextBox
 
 HandleDeckSaveMachineMenu:
 	xor a
@@ -643,8 +636,7 @@ ClearScreenAndDrawDeckMachineScreen:
 	call GetSavedDeckPointers
 	call PrintVisibleDeckMachineEntries
 	call GetSavedDeckCount
-	call EnableLCD
-	ret
+	jp EnableLCD
 
 ; prints wDeckMachineTitleText as title text
 SetDeckMachineTitleText:
@@ -654,8 +646,7 @@ SetDeckMachineTitleText:
 	ld a, [hli]
 	ld h, [hl]
 	ld l, a
-	call ProcessTextFromID
-	ret
+	jp ProcessTextFromID
 
 ; save all sSavedDecks pointers in wMachineDeckPtrs
 GetSavedDeckPointers:
@@ -896,8 +887,7 @@ PrintDeckMachineEntry:
 	jr .loop
 .done
 	ld a, c
-	call DisableSRAM
-	ret
+	jp DisableSRAM
 
 ; counts how many decks in sSavedDecks are not empty
 ; stores value in wNumSavedDecks
@@ -920,8 +910,7 @@ GetSavedDeckCount:
 .got_count
 	ld a, e
 	ld [wNumSavedDecks], a
-	call DisableSRAM
-	ret
+	jp DisableSRAM
 
 ; prints "[wNumSavedDecks]/60"
 PrintNumSavedDecks:
@@ -938,8 +927,7 @@ PrintNumSavedDecks:
 	lb de, 14, 1
 	call InitTextPrinting
 	ld hl, wDefaultText
-	call ProcessText
-	ret
+	jp ProcessText
 
 ; handles player choice in what deck to save
 ; in the Deck Save Machine
@@ -1049,8 +1037,7 @@ CheckIfCanBuildSavedDeck:
 	ld l, a
 	ld bc, DECK_NAME_SIZE
 	add hl, bc
-	call CheckIfHasEnoughCardsToBuildDeck
-	ret
+	jp CheckIfHasEnoughCardsToBuildDeck
 
 ; switches to SRAM bank 0 and stores current SRAM bank in wTempBankSRAM
 ; skips if current SRAM bank is already 0
@@ -1239,8 +1226,7 @@ DrawListScrollArrows:
 	ld a, SYM_BOX_RIGHT
 .got_tile_2
 	lb bc, 19, 11
-	call WriteByteToBGMap0
-	ret
+	jp WriteByteToBGMap0
 
 ; handles the deck menu for when the player
 ; needs to make space for new deck to build
@@ -1484,8 +1470,7 @@ TryBuildDeckMachineDeck:
 	call AddDeckToCollection
 	pop hl
 	ld a, DECK_STRUCT_SIZE
-	call ClearMemory_Bank2
-	ret
+	jp ClearMemory_Bank2
 
 ; collects cards missing from player's collection
 ; and shows its confirmation list
@@ -1881,8 +1866,7 @@ HandleAutoDeckMenu:
 	call .CreateAutoDeckPointerList
 	call PrintVisibleDeckMachineEntries
 	call SafelySwitchToSRAM0
-	call EnableLCD
-	ret
+	jp EnableLCD
 
 ; writes to wMachineDeckPtrs the pointers
 ; to the Auto Decks in sAutoDecks

src/home/substatus.asm
@@ -487,8 +487,7 @@ IsClairvoyanceActive::
 	ccf
 	ret nc
 	ld a, OMANYTE
-	call CountTurnDuelistPokemonWithActivePkmnPower
-	ret
+	jp CountTurnDuelistPokemonWithActivePkmnPower
 
 ; returns carry if turn holder's arena card is paralyzed, asleep, confused,
 ; and/or toxic gas in play (i.e. its pkmn power cannot be used)
@@ -777,8 +776,7 @@ HandleDestinyBondSubstatus::
 	ld l, a
 	call LoadTxRam2
 	ldtx hl, KnockedOutDueToDestinyBondText
-	call DrawWideTextBox_WaitForInput
-	ret
+	jp DrawWideTextBox_WaitForInput
 
 ; when MACHAMP is damaged, if its Strikes Back is active, the
 ; attacking Pokemon (turn holder's arena Pokemon) takes 10 damage.